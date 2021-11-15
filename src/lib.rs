83a1942754f4ef070d1d5a909ad2583fbd6ae8ac
yozhgoor <yozhgoor@outlook.com>
2021-11-15 21:23:01 +0100

    Split imports and add the intro of the documentation

--- src/lib.rs
+++ src/lib.rs
@@ -3,34 +3,52 @@
 // See https://github.com/rust-lang/rust/issues/45127
 #![allow(non_snake_case)]
 
-use std::ffi::{c_void, OsStr, OsString};
-use std::mem::size_of;
-use std::path::{Path, PathBuf};
-use thiserror::Error;
-use windows::Win32::Foundation::{CloseHandle, GetLastError, PWSTR, STATUS_PENDING};
-use windows::Win32::Security::SECURITY_ATTRIBUTES;
-use windows::Win32::System::Threading::{
-    GetExitCodeProcess, TerminateProcess, WaitForSingleObject, PROCESS_CREATION_FLAGS,
-    PROCESS_INFORMATION, STARTUPINFOW, WAIT_OBJECT_0,
-};
-use windows::Win32::System::WindowsProgramming::INFINITE;
-
-#[derive(Error, Debug)]
-pub enum Error {
-    #[error("cannot create process: {0}")]
-    CreationFailed(u32),
-    #[error("cannot get exit status: {0}")]
-    GetExitCodeFailed(u32),
-    #[error("cannot kill process: {0}")]
-    KillFailed(u32),
-}
-
-type Result<T> = std::result::Result<T, Error>;
+//! This crate provide an API similar to `std::process` to create and handle
+//! processes on Windows using the Win32 API through the [windows-rs] crate (see
+//! [this example].
+//!
+//! It's main difference with `std::process::Command` is that it allows running
+//! a command string instead of having to pass the command executable and the
+//! arguments separately.
+//!
+//! This is equivalent of running:
+//!
+//! ```
+//! std::process::Command("cmd.exe")
+//!     .arg("/c")
+//!     .arg(any_command_string)
+//!     .spawn()
+//! ```
+//!
+//! The only difference will be that the `Child` instance will use the PID of
+//! the command instead of the PID of `cmd.exe`. This is important because
+//! calling `.terminate()` in the code above does not work as it kills the PID
+//! of `cmd.exe` instead of the actual command that has been ran.
+//!
+//! # Usage
+//!
+//! Add the following to your `Cargo.toml`:
+//!
+//! ```toml
+//! [dependencies]
+//! create_process_w = { version = "0.1.0", package = "CreateProcessW" }
+//! ```
+//!
+//! You can also use `CreateProcessW` directly, but this doesn't respect Rust's
+//! naming recommendations.
+//!
+use std::ffi::OsString;
+use std::path::PathBuf;
 
 #[derive(Debug)]
+/// a process builder, providing control over how a new process should be
+/// spawned.
 pub struct Command {
+    /// The command line you want to execute
     command: OsString,
+    /// Disable/enable handles inherance
     inherit_handles: bool,
+    /// The full path of the current directory for the process
     current_directory: Option<PathBuf>,
 }
 
@@ -65,6 +83,17 @@ impl Command {
         self.spawn()?.wait()
     }
 }
+
+use std::ffi::{c_void, OsStr};
+use std::mem::size_of;
+use std::path::Path;
+use windows::Win32::Foundation::{GetLastError, PWSTR, STATUS_PENDING};
+use windows::Win32::Security::SECURITY_ATTRIBUTES;
+use windows::Win32::System::Threading::{
+    GetExitCodeProcess, TerminateProcess, WaitForSingleObject, PROCESS_CREATION_FLAGS,
+    PROCESS_INFORMATION, STARTUPINFOW, WAIT_OBJECT_0,
+};
+use windows::Win32::System::WindowsProgramming::INFINITE;
 
 #[derive(Debug)]
 pub struct Child {
@@ -193,6 +222,22 @@ impl ExitStatus {
         self.0
     }
 }
+
+use thiserror::Error;
+
+type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("cannot create process: {0}")]
+    CreationFailed(u32),
+    #[error("cannot get exit status: {0}")]
+    GetExitCodeFailed(u32),
+    #[error("cannot kill process: {0}")]
+    KillFailed(u32),
+}
+
+use windows::Win32::Foundation::CloseHandle;
 
 unsafe fn close_handles(process_info: &PROCESS_INFORMATION) {
     CloseHandle(process_info.hProcess);
